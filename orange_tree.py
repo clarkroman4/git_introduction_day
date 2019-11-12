f48f7b6138f3844c66c778eb01524c8e91542378
Clark Roman <clarkroma4@gmail.com>
2019-11-12 15:53:54 -0600

    Added Oranges to pass growing season

--- orange_tree.py
+++ orange_tree.py
@@ -1,9 +1,14 @@
+import random
+import math
+from oranges import *
+
 class OrangeTree:
   # Trees should start at the age of 0. 
   # Trees should start at a height of 0.
   def __init__(self):
     self.age = 0
     self.height = 0
+    self.oranges = []
 
 
   # Each growing season.
@@ -15,8 +20,16 @@ class OrangeTree:
   def pass_growing_season(self):
     self.age += 1
     self.height += 2.5
-    if self.height > 25:
+    if self.height >=25:
       self.height = 25
+    
+    self.oranges = []
+
+    if self.is_mature():
+      num_oranges = random.randrange(100,301)
+      for num in range(num_oranges):
+        self.oranges.append(Orange)
+
 
 
   # Checks if a tree is old enough to bear fruit ( at least 6 years )
@@ -34,4 +47,4 @@ class OrangeTree:
     pass
 
   def __str__(self):
-    return 'OrangeTree age={0}, height={1}'.format(self.age, self.height)
+    return 'OrangeTree age={0}, height={1}, num_oranges = {2}'.format(self.age, self.height, len(self.oranges))
